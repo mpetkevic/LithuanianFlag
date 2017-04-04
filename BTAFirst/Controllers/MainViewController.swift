63a55ae902d52a4661cc087c15db6bcd4e138fa6
MP Mac <mpmac@mps-macbook-pro.local>
2017-04-04 20:42:16 +0300

    :)

--- BTAFirst/Controllers/MainViewController.swift
+++ BTAFirst/Controllers/MainViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("Labas")
         print("Lietuva")
+        print(":)")
         // Do any additional setup after loading the view.
     }
 
